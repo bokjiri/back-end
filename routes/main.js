23dcc6c23f792fa5cf2abb9e19bb3afcb0c85fa6
pooreumu <pooreum83@gmail.com>
2022-04-25 17:56:44 +0900

    feat(CD): Continuous Deployment
    지속적 배포
     - 스크립트 없이 테스트
    
    2022.04.25 복지리
    commit by pooreumu

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const mainController = require("../controllers/mainController")
 
-router.use("/", mainController.getMains)
+router.use("/", mainController.getMain)
 
 module.exports = router
