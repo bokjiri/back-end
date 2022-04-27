46a3e4ac7034fd9691aedabd720673a642a14f67
pooreumu <pooreum83@gmail.com>
2022-04-27 22:08:52 +0900

    feat(users): putUsers
    유저정보 수정 추가
    
    2022.04.27 복지리
    commit by pooreumu

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,24 @@
+const data = require("../schemas/data")
+const { createUser } = require("../services/user.service")
+
 exports.getUsers = async (req, res) => {
     return res.status(200)
 }
 exports.postUsers = async (req, res) => {
-    return res.status(201)
+    return res.status(200)
 }
 exports.putUsers = async (req, res) => {
-    return res.status(200)
+    try {
+        const { userId } = res.locals
+        const { target, obstacle, lifeCycle } = req.body
+        updateUserInfo({ target, obstacle, lifeCycle, userId })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            result: false,
+            message: "회원가입 중 오류가 발생했습니다.",
+        })
+    }
 }
 exports.deleteUsers = async (req, res) => {
     return res.status(200)
--- services/user.service.js
+++ services/user.service.js
@@ -1,1 +1,9 @@
 const User = require("../schemas/user")
+
+exports.updateUserInfo = (target, obstacle, lifeCycle, userId) => {
+    try {
+        User.updateOne({ userId }, { $set: { target, obstacle, lifeCycle } })
+    } catch (error) {
+        console.log(error)
+    }
+}
