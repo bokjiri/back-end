9ac2f3ca2593f61f2ee36777684d91fa2adcbba6
pooreumu <pooreum83@gmail.com>
2022-04-28 19:37:53 +0900

    feat(auth): split controller
    라우터 컨트롤러 분리
    
    2022.04.28 복지리
    commit by pooreumu

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
-const express = require("express")
 const passport = require("passport")
-const router = express.Router()
-const jwt = require("jsonwebtoken")
+const router = require("express").Router()
+
+const { kakaoCallback } = require("../controllers/user.controller")
 
 router.get("/kakao", passport.authenticate("kakao"))
 
@@ -10,27 +10,6 @@ router.get(
     passport.authenticate("kakao", {
         failureRedirect: "/",
     }),
-    async (req, res) => {
-        const { user } = req.session.passport
-        const { userId, nicname, profileUrl } = user
-        const payload = { userId, nicname, profileUrl }
-        const accessToken = jwt.sign(payload, process.env.ACCESSKEY, {
-            expiresIn: process.env.ATOKENEXPIRE,
-        })
-        const refreshToken = jwt.sign({ email: user }, process.env.REFRESHKEY, {
-            expiresIn: process.env.RTOKENEXPIRE,
-        })
-        res.cookie("ACCESS_TOKEN", accessToken, {
-            sameSite: "None",
-            httpOnly: true,
-            secure: true,
-        })
-            .cookie("REFRESH_TOKEN", refreshToken, {
-                sameSite: "None",
-                httpOnly: true,
-                secure: true,
-            })
-            .redirect("http://localhost:3000")
-    }
+    kakaoCallback
 )
 module.exports = router
