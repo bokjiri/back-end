f3fe65d9b1577a1e425b8c8b029890305aa874cb
pooreumu <pooreum83@gmail.com>
2022-04-28 12:50:45 +0900

    feat(data): create data
    openAPI에서 받아온 데이터 mongodb에 저장
    
    2022.04.28 복지리
    commit by pooreumu

--- openAPI/index.js
+++ openAPI/index.js
@@ -1,21 +1,19 @@
 require("dotenv").config()
 const request = require("request-promise-native")
 const convert = require("xml-js")
+const Data = require("../schemas/data")
+const connect = require("../schemas")
 
-const fs = require("fs")
-fs.truncate("./openAPI/output.txt", () => {
-    console.log("File Content Deleted")
-})
-const myConsole = new console.Console(fs.createWriteStream("./openAPI/output.txt"))
+connect()
+
 let arr = [100, 110, 120, 130, 140, 150, 160, 170, 180]
 let arr1 = ["일자리", "주거", "일상생활", "신체건강 및 보건의료", "정신건강 및 심리정서", "보호 및 돌봄·요양", "보육 및 교육", "문화 및 여가", "안전 및 권익보장"]
-let index = 1
-async function load2(index) {
+async function load2() {
     for (let i = 0; i < arr.length; i++) {
-        await load(arr[i], arr1[i], index)
+        await load(arr[i], arr1[i])
     }
 }
-async function load(asd, zxc, index) {
+async function load(asd, zxc) {
     let url = "http://apis.data.go.kr/B554287/NationalWelfareInformations/NationalWelfarelist"
     let queryParams = "?" + encodeURIComponent("serviceKey") + process.env.SERVICE_KEY /* Service Key*/
     queryParams += "&" + encodeURIComponent("callTp") + "=" + encodeURIComponent("L") /* */
@@ -39,48 +37,46 @@ async function load(asd, zxc, index) {
 
             const servList = jsonParse.wantedList.servList
 
-            await load3(servList, zxc, index)
+            await load3(servList, zxc)
         }
     )
 }
-load2(index)
+load2()
 
-async function load3(servList, zxc, index) {
+async function load3(servList, zxc) {
     for (i of servList) {
-        myConsole.log("index =", index)
-        index++
-        myConsole.log("desireArray =", zxc)
-        myConsole.log("servNm =", i.servNm._text)
-
+        const servId = i.servId._text
+        const desire = zxc
+        const name = i.servNm._text
+        let target
         if (i.trgterIndvdlArray !== undefined) {
-            myConsole.log("trgterIndvdlArray =", i.trgterIndvdlArray._text)
+            target = i.trgterIndvdlArray._text
         } else {
-            myConsole.log("trgterIndvdlArray =", i.trgterIndvdlArray)
         }
+        let obstacle
         if (i.obstKiArray !== undefined) {
-            myConsole.log("obstKiArray =", i.obstKiArray._text)
+            obstacle = i.obstKiArray._text
         } else {
-            myConsole.log("obstKiArray =", i.obstKiArray)
         }
+        let lifeCycle
         if (i.lifeArray !== undefined) {
-            myConsole.log("lifeArray =", i.lifeArray._text)
+            lifeCycle = i.lifeArray._text
         } else {
-            myConsole.log("lifeArray =", i.lifeArray)
         }
-        myConsole.log("servDtlLink =", i.servDtlLink._text)
+        const link = i.servDtlLink._text
 
         let detailUrl = "http://apis.data.go.kr/B554287/NationalWelfareInformations/NationalWelfaredetailed"
         let qqueryParams = "?" + encodeURIComponent("serviceKey") + process.env.SERVICE_KEY
         /* Service Key*/
         qqueryParams += "&" + encodeURIComponent("callTp") + "=" + encodeURIComponent("D") /* */
-        qqueryParams += "&" + encodeURIComponent("servId") + "=" + encodeURIComponent(`${i.servId._text}`) /* */
+        qqueryParams += "&" + encodeURIComponent("servId") + "=" + encodeURIComponent(`${servId}`) /* */
 
         await request(
             {
                 url: detailUrl + qqueryParams,
                 method: "GET",
             },
-            function (error, response, body) {
+            async function (error, response, body) {
                 const result = body
                 const xmlToJson = convert.xml2json(result, {
                     compact: true,
@@ -88,17 +84,13 @@ async function load3(servList, zxc, index) {
                 })
                 const jsonParse = JSON.parse(xmlToJson)
                 const a = jsonParse.wantedDtl.tgtrDtlCn._text.trim()
-                const b = jsonParse.wantedDtl.slctCritCn._text.trim()
                 const c = jsonParse.wantedDtl.alwServCn._text.trim()
                 const d = jsonParse.wantedDtl.jurMnofNm._text
 
-                myConsole.log("jurMnofNm =", d)
-                myConsole.log("tgtrDtlCn =", a)
-                myConsole.log("\n")
-                // myConsole.log("slctCritCn =", b)
-                // myConsole.log("\n")
-                myConsole.log("alwServCn =", c)
-                myConsole.log("\n")
+                const institution = d
+                const summary = a
+                const support = c
+                await Data.create({ desire, name, target, obstacle, lifeCycle, link, institution, summary, support })
             }
         )
     }
