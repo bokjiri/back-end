13aa56f56029e6ff1683a9bc53cd2e16c3b35790
rlasuddn <rlasuddn>
2022-05-02 22:16:03 +0900

    feat(tip):add tip
     -버그 제보 기능 추가
     -버그 제보 데이터 txt 파일에 저장
    2022.05.02 복세편살
    commit by youngwoo

--- controllers/tip.controller.js
+++ controllers/tip.controller.js
@@ -1,3 +1,6 @@
+const { postTip } = require("../services/tip.service")
 exports.postTips = async (req, res) => {
-    return res.status(200)
+    const { userId, dataId } = req.body
+    postTip(userId, dataId)
+    res.status(200).json({ message: "버그제보를 완료했습니다." })
 }
