c3f02c2e542261fb365d1fc983aea213f0a405af
zzpjw <zzpjw9710@naver.com>
2022-05-04 15:22:38 +0900

    fix(mainAPI): getMain & add swagger, unit test
    main GET API 수정
     - query로 필수입력값 입력하지 않으면 에러 추가
     - DB 조회 실패 에러 추가
     - DB/Data 한번만 조회하게끔 수정
     - 이후 코드 리펙토링이 수월하게끔 일관된 코드로 수정
    unit test code 추가
    swagger 추가
    
    2022.05.04 복세편살
    commit by zzpjw

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,19 +1,66 @@
-const { checkUserData } = require("../services/main.service")
+const { checkUserData, checkUserId, checkBokjiApi } = require("../services/main.service")
 
 exports.getMain = async (req, res) => {
+    /*========================================================================================================
+    #swagger.tags = ['main']
+    #swagger.summary = '정책 추천 API'
+    #swagger.description = '내 정보와 일치하는 정책 목록을 조회하는 API'
+    ========================================================================================================*/
+    if (!req.query.userId) {
+        /*=====================================================================================
+        #swagger.responses[400] = {
+            description: 'userId가 query로 입력되지 않았을 때, 아래 예제와 같은 형태로 응답받습니다.',
+            schema: { "result": "FAIL", 'code': -10, 'message': "필수 입력값 조회 실패" }
+        }
+        =====================================================================================*/
+        return res.status(400).json({ result: "Fail", code: -10, message: "필수 입력값 조회 실패" })
+    }
     const { userId } = req.query
+    const isUser = await checkUserId(userId)
+    if (!isUser) {
+        /*=====================================================================================
+        #swagger.responses[400] = {
+            description: '입력 받은 userId가 DB에 존재하지 않을 때, 아래 예제와 같은 형태로 응답받습니다.',
+            schema: { "result": "FAIL", 'code': -11, 'message': "데이터베이스 조회 실패" }
+        }
+        =====================================================================================*/
+        return res.status(400).json({ result: "FAIL", code: -11, message: "데이터베이스 조회 실패" })
+    }
+    const isData = await checkBokjiApi()
+    if (!isData) {
+        /*=====================================================================================
+        #swagger.responses[400] = {
+            description: 'BokjiApi가 DB에 존재하지 않을 때, 아래 예제와 같은 형태로 응답받습니다.',
+            schema: { "result": "FAIL", 'code': -11, 'message': "데이터베이스 조회 실패" }
+        }
+        =====================================================================================*/
+        return res.status(400).json({ result: "FAIL", code: -11, message: "데이터베이스 조회 실패" })
+    }
     try {
-        let [checkedData] = await checkUserData(userId)
+        let checkedData = await checkUserData(isUser, isData)
+        //checkedData는 배열 안에 오브젝트(정책 하나)가 들어가있어야 한다.
+        /*=====================================================================================
+        #swagger.responses[200] = {
+            description: '정상적으로 값을 받았을 때, 아래 예제와 같은 형태로 응답받습니다.',
+            schema: { result: "SUCCESS", message: "메인 페이지 추천 정책 조회 성공", checkedData }
+        }
+        =====================================================================================*/
         return res.status(200).json({
             result: "SUCCESS",
-            message: "추천 조회 성공",
+            message: "메인 페이지 추천 정책 조회 성공",
             checkedData,
         })
     } catch (error) {
         console.log(error)
-        res.status(400).json({
+        /*=====================================================================================
+        #swagger.responses[200] = {
+            description: '정상적으로 값을 받았을 때, 아래 예제와 같은 형태로 응답받습니다.',
+            schema: { result: "SUCCESS", message: "메인 페이지 추천 정책 조회 실패" }
+        }
+        =====================================================================================*/
+        return res.status(400).json({
             result: "FAIL",
-            message: "실패",
+            message: "메인 페이지 추천 정책 조회 실패",
         })
     }
 }
--- controllers/specs/main.spec.js
+++ controllers/specs/main.spec.js
@@ -1,14 +1,12 @@
 const mainController = require("../main.controller")
 const httpMocks = require("node-mocks-http")
-const mainData = require("../../test/data/new-main.json")
+// const mainData = require("../../test/data/new-main.json")
 jest.mock("../../schemas/user")
 jest.mock("../../schemas/data")
 const User = require("../../schemas/user")
 const BokjiApi = require("../../schemas/data")
+const request = require("request")
 const userId = { userId: 1 }
-// const mark = [1, 3, 5, 8, 9]
-// const dataId = [1, 3, 5, 8, 9]
-// const beforeMarks = [4, 8, 9, 5]
 
 let req, res
 beforeEach(() => {
@@ -16,34 +14,50 @@ beforeEach(() => {
     res = httpMocks.createResponse()
 })
 
-// describe("메인페이지 테스트 코드", () => {
-//     describe("getMain 테스트", () => {
-//         it("User find 가 잘 실행이되나?", async () => {
-//             req.query.userId = userId
-//             const option = { _id: false, lifeCycle: true, target: true, obstacle: true }
-//             await mainController.getMain(req, res)
-//             expect(User.find).toBeCalledWith({ userId }, option)
-//         })
-//         it("BokjiAPI find 가 잘 실행이되나?", async () => {
-//             req.query.userId = userId
-//             User.find.mockReturnValue(lifeCycle)
-//             findData = { dataId: mark.mark }
-//             await mainController.getMain(req, res)
-//             expect(BokjiApi.find).toBeCalledWith(findData, option)
-//         })
-//         it("showMark가 400과 응답값을 잘 리턴하는가?", async () => {
-//             BokjiApi.find.mockReturnValue(null)
-//             await markController.getMarks(req, res)
-//             const markResult = { message: "북마크 데이터를 받아 오지 못했습니다." }
-//             expect(res.statusCode).toBe(400)
-//             expect(res._getJSONData()).toStrictEqual(markResult)
-//         })
-//         it("showMark가 200과 응답값을 잘 리턴하는가?", async () => {
-//             User.findOne.mockReturnValue(mark)
-//             BokjiApi.find.mockReturnValue(markData)
-//             await markController.getMarks(req, res)
-//             expect(res.statusCode).toBe(200)
-//             expect(res._getJSONData()).toStrictEqual({ userMark: markData })
-//         })
-//     })
-// })
+describe("메인 페이지 테스트", () => {
+    describe("getMain API 테스트", () => {
+        describe("필수 입력값 조회 테스트", () => {
+            it("userId가 false로 들어오면 400과 응답값을 잘 리턴하는가?", async () => {
+                req.query.userId = false
+                await mainController.getMain(req, res)
+                let userIdResult = { result: "Fail", code: -10, message: "필수 입력값 조회 실패" }
+
+                expect(res.statusCode).toBe(400)
+                expect(res._getJSONData()).toStrictEqual(userIdResult)
+            })
+        })
+        describe("데이터베이스 조회 테스트", () => {
+            it("UserId가 DB에 존재하지 않는다면 400과 응답값을 잘 리턴하는가?", async () => {
+                req.query.userId = userId
+                User.findOne.mockReturnValue(false)
+                option = { _id: false, lifeCycle: true, target: true, obstacle: true }
+                await mainController.getMain(req, res)
+                let isUserResult = { result: "FAIL", code: -11, message: "데이터베이스 조회 실패" }
+                expect(User.find).toBeCalledWith({ userId }, option)
+                expect(res.statusCode).toBe(400)
+                expect(res._getJSONData()).toStrictEqual(isUserResult)
+            })
+            it("BokjiApi가 DB에 존재하지 않는다면 400과 응답값을 잘 리턴하는가?", async () => {
+                req.query.userId = userId
+                BokjiApi.find.mockReturnValue(false)
+                const isDataResult = { result: "FAIL", code: -11, message: "데이터베이스 조회 실패" }
+                await mainController.getMain(req, res)
+                expect(res.statusCode).toBe(400)
+                expect(res._getJSONData()).toStrictEqual(isDataResult)
+            })
+        })
+        describe("API 테스트", () => {
+            // it("getMain이 200과 응답값을 잘 리턴하는가?", async () => {
+            //     User.findOne.mockReturnValue(true)
+            //     BokjiApi.find.mockReturnValue(true)
+            //     await mainController.getMain(req, res)
+            //     expect(res.statusCode).toBe(200)
+            //     expect(res._getJSONData()).toStrictEqual({
+            //         result: "SUCCESS",
+            //         message: "메인 페이지 추천 정책 조회 성공",
+            //         checkedData,
+            //     })
+            // })
+        })
+    })
+})
