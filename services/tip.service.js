eb99ec298f13fdaf71baafccc1945c958c9ce96c
rlasuddn <rlasuddn>
2022-05-04 17:40:04 +0900

    fix(tip):txt file name 변경
     - tip로그 텍스트 파일명 변경
    2022.05.04 복세편살
    commit by youngwoo

--- services/tip.service.js
+++ services/tip.service.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 exports.postTip = (userId, dataId) => {
     const data = "\nuserId: " + userId.toString() + " dataId: " + dataId.toString()
 
-    fs.writeFile("./tipLog.txt", data, { flag: "a+" }, (err) => {
+    fs.writeFile("./tip_log.txt", data, { flag: "a+" }, (err) => {
         if (err) {
             console.error(err)
             return
