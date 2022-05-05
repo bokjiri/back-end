a6c00ee0110c30a606bc0d9b681355c942449d85
rlasuddn <rlasuddn>
2022-05-05 17:06:45 +0900

    fix(news): news 기능 수정
     - news Collection 생성
     - 뉴스 코드 수정
    2022.어린이날
    commit by youngwoo

---
+++ schemas/news.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const autoIdSetter = require("./autoidsetter")
+
+const news = mongoose.Schema({
+    title: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    desc: {
+        type: String,
+    },
+    date: {
+        type: String,
+    },
+})
+autoIdSetter(news, mongoose, "news", "newsId")
+module.exports = mongoose.model("news", news)
