4ffbfb5253a369a089870671b24f788fc1021372
zzpjw <zzpjw9710@naver.com>
2022-05-09 15:33:14 +0900

    fix(swagger): create swagger
    create swagger 배포용 url 설정
    
    2022.05.09 복세편살
    commit by zzpjw

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  host: "localhost:3000",
-  schemes: ["http"],
+  host: "pooreum.shop",
+  schemes: ["https"],
 };
 
 const outputFile = "./swagger-output.json";
