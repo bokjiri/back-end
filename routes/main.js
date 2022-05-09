ca8ad52f559559d011b4458b4f2c653f6dfb1667
zzpjw <zzpjw9710@naver.com>
2022-05-09 15:49:20 +0900

    fix(swagger): create swagger
    fix main api for swagger
    
    2022.05.09 복세편살
    commit by zzpjw

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const mainController = require("../controllers/main.controller")
 
-router.use("/:userId", mainController.getMain)
+router.get("/:userId", mainController.getMain)
 
 module.exports = router
