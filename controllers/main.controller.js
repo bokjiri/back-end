aa6b8a696bc5fd46200d5b4011c261554c09450a
zzpjw <zzpjw9710@naver.com>
2022-05-09 21:54:38 +0900

    fix(mainAPI): modify getMain
    main GET API 수정
    - DB를 거치지 않는 service 파일의 함수를 controller로 병합
    - trycatch 수정
    
    2022.05.09 복세편살
    commit by zzpjw

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,4 +1,3 @@
-const e = require("express")
 const { checkUserData, checkUserId, checkBokjiApi } = require("../services/main.service")
 
 exports.getMain = async (req, res) => {
@@ -7,72 +6,119 @@ exports.getMain = async (req, res) => {
     #swagger.summary = '추천 정책 카테고리별 조회'
     #swagger.description = '내 정보와 일치하는 정책 목록을 카테고리별로 모두 조회한다.'
     ========================================================================================================*/
-    if (!req.params.userId) {
-        /*=====================================================================================
+    try {
+        if (!req.params.userId) {
+            /*=====================================================================================
         #swagger.responses[400] = {
             description: 'userId가 params로 입력되지 않았을 때, 아래 예제와 같은 형태로 응답받습니다.',
             schema: { result: "FAIL", code: -10, message: "필수 입력값 조회 실패" }
         }
         =====================================================================================*/
-        return res.status(400).json({ result: "Fail", code: -10, message: "필수 입력값 조회 실패" })
-    }
-    const { userId } = req.params
-    const isUser = await checkUserId(userId)
-    if (!isUser) {
-        /*=====================================================================================
+            return res.status(400).json({ result: "Fail", code: -10, message: "필수 입력값 조회 실패" })
+        }
+        const { userId } = req.params
+        const isUser = await checkUserId(userId)
+        if (!isUser) {
+            /*=====================================================================================
         #swagger.responses[404] = {
             description: '입력 받은 userId가 DB에 존재하지 않을 때, 아래 예제와 같은 형태로 응답받습니다.',
             schema: { result: "FAIL", code: -11, message: "데이터베이스 조회 실패" }
         }
         =====================================================================================*/
-        return res.status(404).json({ result: "FAIL", code: -11, message: "데이터베이스 조회 실패" })
-    }
-    const allData = await checkBokjiApi()
-    if (!allData) {
-        /*=====================================================================================
+            return res.status(404).json({ result: "FAIL", code: -11, message: "데이터베이스 조회 실패" })
+        }
+        const allData = await checkBokjiApi()
+        if (!allData) {
+            /*=====================================================================================
         #swagger.responses[404] = {
             description: 'BokjiApi가 DB에 존재하지 않을 때, 아래 예제와 같은 형태로 응답받습니다.',
             schema: { result: "FAIL", code: -11, message: "데이터베이스 조회 실패" }
         }
         =====================================================================================*/
-        return res.status(404).json({ result: "FAIL", code: -11, message: "데이터베이스 조회 실패" })
-    }
-    let isData = []
-    allData.forEach((data)=>{
-        if(data.lifeCycle[0] !== undefined || data.target[0] !== undefined || data.obstacle[0] !== undefined ){
-            isData.push(data)
-        } 
-    })
-    try {
-        let checkedData = await checkUserData(isUser, isData)
+            return res.status(404).json({ result: "FAIL", code: -11, message: "데이터베이스 조회 실패" })
+        }
+        const isData = allData
+        //-----------------------------------------------------------추천 로직-----------------------------------------------------------------//
+        let checkedWithLifeCycle = []
+        //만약 isUser에 lifecycle이 존재한다면
+        if (isUser.lifeCycle.length !== 0) {
+            for (let i = 0; i < isUser.lifeCycle.length; i++) {
+                for (let j = 0; j < isData.length; j++) {
+                    if (isData[j].lifeCycle.includes(isUser.lifeCycle[i]) === true || isData[j].lifeCycle[0] === undefined) {
+                        checkedWithLifeCycle.push(isData[j])
+                    }
+                }
+            }
+        }
+        //만약 isUser에 lifecycle이 존재하지 않는다면
+        else {
+            checkedWithLifeCycle = isData
+        } // console.log(checkedWithLifeCycle)
+
+        let checkedWithTarget = []
+        //만약 isUser에 target이 존재한다면
+        if (isUser.target.length !== 0) {
+            for (let i = 0; i < isUser.target.length; i++) {
+                for (let j = 0; j < checkedWithLifeCycle.length; j++) {
+                    if (checkedWithLifeCycle[j].target.includes(isUser.target[i]) === true || checkedWithLifeCycle[j].target[0] === undefined) {
+                        checkedWithTarget.push(checkedWithLifeCycle[j])
+                    }
+                }
+            }
+        }
+        //만약 isUser에 target이 존재하지 않는다면
+        else {
+            checkedWithTarget = checkedWithLifeCycle
+        } // console.log(checkedWithTarget)
+
+        //만약 isUser에 obstacle이 존재한다면
+        let checkedWithObstacle = []
+        if (isUser.obstacle.length !== 0) {
+            for (let i = 0; i < isUser.obstacle.length; i++) {
+                for (let j = 0; j < checkedWithTarget.length; j++) {
+                    if (checkedWithTarget[j].obstacle.includes(isUser.target[i]) === true || checkedWithTarget[j].obstacle[0] === undefined) {
+                        checkedWithObstacle.push(checkedWithTarget[j])
+                    }
+                }
+            }
+        }
+        //만약 isUser에 obstacle이 존재하지 않는다면
+        else {
+            checkedWithObstacle = checkedWithTarget
+        } // console.log(checkedWithObstacle)
+
+        // let checkedData = await checkUserData(isUser, isData)   -> DB를 탄다면 사용
+        let result = checkedWithObstacle.filter((v, i) => checkedWithObstacle.indexOf(v).name === i.name)
+        let checkedData = result
+        //----------------------------------------------------------------------------------------------------------------------------//
         let work = []
         let houseLife = []
         let health = []
         let eduCare = []
         let safetyRight = []
         let etc = []
-        for (let i = 0; i < checkedData.length; i++){
-            if(checkedData[i].desire === "일자리"){
+        for (let i = 0; i < checkedData.length; i++) {
+            if (checkedData[i].desire === "일자리") {
                 work.push(checkedData[i])
             }
-            if(checkedData[i].desire === "주거 및 일상생활"){
+            if (checkedData[i].desire === "주거 및 일상생활") {
                 houseLife.push(checkedData[i])
             }
-            if(checkedData[i].desire === "건강"){
+            if (checkedData[i].desire === "건강") {
                 health.push(checkedData[i])
             }
-            if(checkedData[i].desire === "교육 및 돌봄"){
+            if (checkedData[i].desire === "교육 및 돌봄") {
                 eduCare.push(checkedData[i])
             }
-            if(checkedData[i].desire === "안전 및 권익보장"){
+            if (checkedData[i].desire === "안전 및 권익보장") {
                 safetyRight.push(checkedData[i])
             }
-            if(checkedData[i].desire === "기타"){
+            if (checkedData[i].desire === "기타") {
                 etc.push(checkedData[i])
             }
         }
         //checkedData는 배열 안에 오브젝트(정책 하나)가 들어가있어야 한다.
-        
+
         /*=====================================================================================
         #swagger.responses[200] = {
             description: '정상적으로 값을 받았을 때, 아래 예제와 같은 형태로 응답받습니다.',
@@ -96,10 +142,10 @@ exports.getMain = async (req, res) => {
             health,
             eduCare,
             safetyRight,
-            etc
+            etc,
         })
     } catch (error) {
-        console.log(error)
+        console.error(error)
         /*=====================================================================================
         #swagger.responses[400] = {
             description: '정상적으로 값을 받지 못했을 때, 아래 예제와 같은 형태로 응답받습니다.',
