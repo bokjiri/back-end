2c0c6b3b4bd12fe7d5119ce3d5baaba8e4d2af2c
rlasuddn <rlasuddn>
2022-05-12 17:10:16 +0900

    feat(검색): 검색기능 1차 완료!
    2022.05.12 복세편살
    commit by youngwoo

--- services/search.service.js
+++ services/search.service.js
@@ -9,26 +9,21 @@ exports.searchService = async (userId, searchKey, type) => {
         let dataList = []
 
         dataList.push(...UserData)
-        const search = new RegExp(searchKey)
+        const search = new RegExp(searchKey, "g")
 
         let searchList = []
+
         for (let i of dataList) {
             if (type === "전체") {
                 if (search.test(i.name) || search.test(i.summary) || search.test(i.desire)) {
                     console.log("전체")
                     searchList.push(i)
                 }
-            }
-            if (type === "정책명" && search.test(i.name)) {
-                console.log("정책명")
+            } else if (type === "정책명" && search.test(i.name)) {
                 searchList.push(i)
-            }
-            if (type === "내용" && search.test(i.summary)) {
-                console.log("내용")
+            } else if (type === "내용" && search.test(i.summary)) {
                 searchList.push(i)
-            }
-            if (type === "카테고리" && search.test(i.desire)) {
-                console.log("카테고리")
+            } else if (type === "정책분야" && search.test(i.desire)) {
                 searchList.push(i)
             }
         }
