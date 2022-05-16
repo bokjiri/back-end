09c3946db24271a602ce733ff9ae6f3cb8cf618a
pooreumu <pooreum83@gmail.com>
2022-05-16 12:05:35 +0900

    feat(openAPI): axios로 변경
    axios로 변경
    
    2022.05.13 복세편살
    commit by pooreumu

--- openAPI/index.js
+++ openAPI/index.js
@@ -1,52 +1,43 @@
 require("dotenv").config()
-const request = require("request-promise-native")
+const axios = require("axios")
 const convert = require("xml-js")
 const Data = require("../schemas/data")
 const connect = require("../schemas")
 
 connect()
 
-let arr = [100, 110, 120, 130, 140, 150, 160, 170, 180]
-let arr1 = ["일자리", "주거 및 일상생활", "주거 및 일상생활", "건강", "건강", "교육 및 돌봄", "교육 및 돌봄", "기타", "안전 및 권익보장"]
-function load2() {
-    for (let i = 0; i < arr.length; i++) {
-        load(arr[i], arr1[i])
+const desireCode = [100, 110, 120, 130, 140, 150, 160, 170, 180]
+const desireName = ["일자리", "주거 및 일상생활", "주거 및 일상생활", "건강", "건강", "교육 및 돌봄", "교육 및 돌봄", "기타", "안전 및 권익보장"]
+
+loadOpenApi()
+
+async function loadOpenApi() {
+    for (let i = 0; i < desireCode.length; i++) {
+        let url = "http://apis.data.go.kr/B554287/NationalWelfareInformations/NationalWelfarelist"
+        let queryParams = "?" + encodeURIComponent("serviceKey") + process.env.SERVICE_KEY /* Service Key*/
+        queryParams += "&" + encodeURIComponent("callTp") + "=" + encodeURIComponent("L") /* */
+        queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1") /* */
+        queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("500") /* */
+        queryParams += "&" + encodeURIComponent("srchKeyCode") + "=" + encodeURIComponent("003") /* */
+        queryParams += "&" + encodeURIComponent("desireArray") + "=" + encodeURIComponent(desireCode[i]) /* */
+
+        const response = await axios.get(url + queryParams)
+        const data = response.data
+        const xmlToJson = convert.xml2json(data, {
+            compact: true,
+            space: 4,
+        })
+        const jsonParse = JSON.parse(xmlToJson)
+        const servList = jsonParse.wantedList.servList
+
+        await getDetail(servList, desireName[i])
     }
 }
-async function load(asd, zxc) {
-    let url = "http://apis.data.go.kr/B554287/NationalWelfareInformations/NationalWelfarelist"
-    let queryParams = "?" + encodeURIComponent("serviceKey") + process.env.SERVICE_KEY /* Service Key*/
-    queryParams += "&" + encodeURIComponent("callTp") + "=" + encodeURIComponent("L") /* */
-    queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1") /* */
-    queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("500") /* */
-    queryParams += "&" + encodeURIComponent("srchKeyCode") + "=" + encodeURIComponent("003") /* */
-    queryParams += "&" + encodeURIComponent("desireArray") + "=" + encodeURIComponent(`${asd}`) /* */
 
-    await request(
-        {
-            url: url + queryParams,
-            method: "GET",
-        },
-        async function (error, response, body) {
-            const result = body
-            const xmlToJson = convert.xml2json(result, {
-                compact: true,
-                space: 4,
-            })
-            const jsonParse = JSON.parse(xmlToJson)
-
-            const servList = jsonParse.wantedList.servList
-
-            await load3(servList, zxc)
-        }
-    )
-}
-load2()
-
-async function load3(servList, zxc) {
+async function getDetail(servList, desireName) {
     for (i of servList) {
         const servId = i.servId._text
-        const desire = zxc
+        const desire = desireName
         const name = i.servNm._text
         let target
         if (i.trgterIndvdlArray !== undefined) {
@@ -72,37 +63,29 @@ async function load3(servList, zxc) {
         const link = i.servDtlLink._text
 
         let detailUrl = "http://apis.data.go.kr/B554287/NationalWelfareInformations/NationalWelfaredetailed"
-        let qqueryParams = "?" + encodeURIComponent("serviceKey") + process.env.SERVICE_KEY
-        /* Service Key*/
+        let qqueryParams = "?" + encodeURIComponent("serviceKey") + process.env.SERVICE_KEY /* Service Key*/
         qqueryParams += "&" + encodeURIComponent("callTp") + "=" + encodeURIComponent("D") /* */
         qqueryParams += "&" + encodeURIComponent("servId") + "=" + encodeURIComponent(`${servId}`) /* */
 
-        await request(
-            {
-                url: detailUrl + qqueryParams,
-                method: "GET",
-            },
-            async function (error, response, body) {
-                const result = body
-                const xmlToJson = convert.xml2json(result, {
-                    compact: true,
-                    space: 4,
-                })
-                const jsonParse = JSON.parse(xmlToJson)
-                let summary
-                let support
+        const response = await axios.get(detailUrl + queryParams)
+        const data = response.data
+        const xmlToJson = convert.xml2json(data, {
+            compact: true,
+            space: 4,
+        })
+        const jsonParse = JSON.parse(xmlToJson)
+        let summary
+        let support
 
-                if (jsonParse.wantedDtl.tgtrDtlCn !== undefined) {
-                    summary = jsonParse.wantedDtl.tgtrDtlCn._text.trim()
-                }
-                if (jsonParse.wantedDtl.alwServCn !== undefined) {
-                    support = jsonParse.wantedDtl.alwServCn._text.trim()
-                }
+        if (jsonParse.wantedDtl.tgtrDtlCn !== undefined) {
+            summary = jsonParse.wantedDtl.tgtrDtlCn._text.trim()
+        }
+        if (jsonParse.wantedDtl.alwServCn !== undefined) {
+            support = jsonParse.wantedDtl.alwServCn._text.trim()
+        }
 
-                const institution = jsonParse.wantedDtl.jurMnofNm._text
+        const institution = jsonParse.wantedDtl.jurMnofNm._text
 
-                await Data.create({ desire, name, target, obstacle, lifeCycle, link, institution, summary, support })
-            }
-        )
+        await Data.create({ desire, name, target, obstacle, lifeCycle, link, institution, summary, support })
     }
 }
