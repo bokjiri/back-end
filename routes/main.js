603e54a3cef8eb8bedee84cde4db091b47ff58c5
zzpjw <zzpjw9710@naver.com>
2022-05-18 22:19:54 +0900

    fix(getMain): import authMiddleware
    메인 페이지 authMiddleware 추가
    - import
    
    2022.05.18 복세편살
    commit by zzpjw

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const mainController = require("../controllers/main.controller")
+const authMiddleware = require("../middlewares/auth/auth.middleware")
 
 router.get("/:userId", authMiddleware, mainController.getMain)
 
