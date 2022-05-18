3616d0514a788d22332f9089c119a171f48847b6
zzpjw <zzpjw9710@naver.com>
2022-05-18 22:15:06 +0900

    feat(getMain): authMiddleware 추가
    메인 페이지 authMiddleware 추가
    
    2022.05.18 복세편살
    commit by zzpjw

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const mainController = require("../controllers/main.controller")
 
-router.get("/:userId", mainController.getMain)
+router.get("/:userId", authMiddleware, mainController.getMain)
 
 module.exports = router
