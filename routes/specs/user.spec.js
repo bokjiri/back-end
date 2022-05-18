69b3637ed98f754ad92d43e7b8c07e368dd32931
pooreumu <pooreum83@gmail.com>
2022-05-18 21:26:23 +0900

    feat(user): 통합테스트 추가
    통합테스트 조금 추가
    
    2022.05.18 복세편살
    commit by pooreumu

--- routes/specs/user.spec.js
+++ routes/specs/user.spec.js
@@ -1,19 +1,53 @@
 const request = require("supertest")
 const app = require("../../app")
 const agent = request.agent(app)
-const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
+const User = require("../../schemas/user")
 
-// describe("user 통합 테스트", () => {
-//     describe("get /api/users/:userId", () => {
-//         it("get /api/users/:userId 잘 되나?", async () => {
-//             const userId = "73"
-//             const res = await request(app)
-//                 .get(`/api/users/${userId}`)
-//                 .set(
-//                     "Authorization",
-//                     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjczLCJuaWNrbmFtZSI6IuydtO2RuOumhCIsInByb2ZpbGVVcmwiOiJodHRwOi8vay5rYWthb2Nkbi5uZXQvZG4vZHBrOWwxL2J0cW1HaEEybEtML096MHdEdUpuMVlWMkRJbjkyZjZEVksvaW1nXzY0MHg2NDAuanBnIiwiZW1haWwiOiJwb29yZXVtODNAZ21haWwuY29tIiwiaWF0IjoxNjUyNzk3NTkwLCJleHAiOjE2NTI4ODM5OTB9.jZxkdklHbQKx9xgeaLf8wMPJXRPzCsoGrxKuZ5lQkUo"
-//                 )
-//             expect(res.statusCode).toBe(201)
-//         })
-//     })
-// })
+describe("user 통합 테스트", () => {
+    beforeAll(async () => {
+        await mongoose
+            .connect(`mongodb://${process.env.DBID}:${process.env.DBPW}@3.36.130.225:27017/test?authSource=admin&authMechanism=SCRAM-SHA-1`, { ignoreUndefined: true })
+            .then(() => {
+                console.log("testdb 연결완료")
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    })
+    afterAll(async () => {
+        await User.deleteMany()
+    })
+    describe("post /api/users", () => {
+        it("post /api/users 회원가입 잘됨?", async () => {
+            const email = "email@email.com"
+            const nickname = "asdf"
+            const password = "asdf"
+            const res = await request(app).post(`/api/users`).send({ email, nickname, password })
+            expect(res.statusCode).toBe(201)
+        })
+    })
+    describe("post /api/users/auth", () => {
+
+        const email = "email@email.com"
+        const password = "asdf"
+        const res = await request(app).post(`/api/users/auth`).send({ email, password })
+        console.log(res)
+
+        it("post /api/users/auth 로그인 잘됨?", async () => {
+            // expect(res.statusCode).toBe(200)
+        })
+    })
+    //     describe("get /api/users/:userId", () => {
+    //         it("get /api/users/:userId 잘 되나?", async () => {
+    //             const userId = "73"
+    //             const res = await request(app)
+    //                 .get(`/api/users/${userId}`)
+    //                 .set(
+    //                     "Authorization",
+    //                     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjczLCJuaWNrbmFtZSI6IuydtO2RuOumhCIsInByb2ZpbGVVcmwiOiJodHRwOi8vay5rYWthb2Nkbi5uZXQvZG4vZHBrOWwxL2J0cW1HaEEybEtML096MHdEdUpuMVlWMkRJbjkyZjZEVksvaW1nXzY0MHg2NDAuanBnIiwiZW1haWwiOiJwb29yZXVtODNAZ21haWwuY29tIiwiaWF0IjoxNjUyNzk3NTkwLCJleHAiOjE2NTI4ODM5OTB9.jZxkdklHbQKx9xgeaLf8wMPJXRPzCsoGrxKuZ5lQkUo"
+    //                 )
+    //             expect(res.statusCode).toBe(201)
+    //         })
+    //     })
+})
