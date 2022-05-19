5a1ad1363b2efc9a06e0684cd33d6d801cadb381
pooreumu <pooreum83@gmail.com>
2022-05-19 17:49:42 +0900

    feat(user): 통합테스트 추가
    통합테스트 추가
    
    2022.05.19 복세편살
    commit by pooreumu

--- routes/specs/user.spec.js
+++ routes/specs/user.spec.js
@@ -1,9 +1,10 @@
 const request = require("supertest")
 const app = require("../../app")
-const agent = request.agent(app)
 const mongoose = require("mongoose")
 const User = require("../../schemas/user")
 const redis = require("../../schemas/redis")
+const jwt = require("jsonwebtoken")
+const patchUserData = require("../../test/user/patch.json")
 
 beforeAll(async () => {
     await mongoose
@@ -20,39 +21,51 @@ afterAll(async () => {
     await mongoose.disconnect()
     await redis.quit()
 })
-describe("user 통합 테스트", () => {
-    describe("post /api/users", () => {
-        it("post /api/users 회원가입 잘됨?", async () => {
-            const email = "email@email.com"
-            const nickname = "asdf"
-            const password = "asdf"
-            const res = await request(app).post(`/api/users`).send({ email, nickname, password })
-            expect(res.statusCode).toBe(201)
-        })
-    })
-    describe("post /api/users/auth", () => {
-        beforeEach(async () => {
-            const email = "email@email.com"
-            const password = "asdf"
-            const res = await request(app).post(`/api/users/auth`).send({ email, password })
-            const aToken = res.body.accessToken
-            const rToken = res.body.accessToken
-        })
 
-        it("post /api/users/auth 로그인 잘됨?", async () => {
-            // expect(res.statusCode).toBe(200)
-        })
+describe("post /api/users", () => {
+    it("post /api/users 회원가입 잘됨?", async () => {
+        const email = "email@email.com"
+        const nickname = "asdf"
+        const password = "asdf"
+        const res = await request(app).post(`/api/users`).send({ email, nickname, password })
+        expect(res.statusCode).toBe(201)
     })
-    //     describe("get /api/users/:userId", () => {
-    //         it("get /api/users/:userId 잘 되나?", async () => {
-    //             const userId = "73"
-    //             const res = await request(app)
-    //                 .get(`/api/users/${userId}`)
-    //                 .set(
-    //                     "Authorization",
-    //                     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjczLCJuaWNrbmFtZSI6IuydtO2RuOumhCIsInByb2ZpbGVVcmwiOiJodHRwOi8vay5rYWthb2Nkbi5uZXQvZG4vZHBrOWwxL2J0cW1HaEEybEtML096MHdEdUpuMVlWMkRJbjkyZjZEVksvaW1nXzY0MHg2NDAuanBnIiwiZW1haWwiOiJwb29yZXVtODNAZ21haWwuY29tIiwiaWF0IjoxNjUyNzk3NTkwLCJleHAiOjE2NTI4ODM5OTB9.jZxkdklHbQKx9xgeaLf8wMPJXRPzCsoGrxKuZ5lQkUo"
-    //                 )
-    //             expect(res.statusCode).toBe(201)
-    //         })
-    //     })
 })
+describe("유저 통합테스트", () => {
+    let aToken, rToken, userId, Authorization, reAuthorization
+    beforeEach(async () => {
+        const email = "email@email.com"
+        const password = "asdf"
+        const res = await request(app).post(`/api/users/auth`).send({ email, password })
+        aToken = res.body.accessToken
+        rToken = res.body.accessToken
+        Authorization = `Bearer ${aToken}`
+        reAuthorization = `Bearer ${rToken}`
+        userId = jwt.decode(aToken).userId
+    })
+
+    it("get /api/users/:userId 유저정보 잘 불러와짐?", async () => {
+        const res = await request(app).get(`/api/users/${userId}`).set({ Authorization, reAuthorization })
+        expect(res.statusCode).toBe(201)
+    })
+    it("patch /api/users/:userId 유저정보 잘 수정됨?", async () => {
+        const res = await request(app).patch(`/api/users/${userId}`).set({ Authorization, reAuthorization }).send(patchUserData)
+        expect(res.statusCode).toBe(201)
+    })
+    it("delete /api/users/:userId 유저정보 잘 지워짐", async () => {
+        const res = await request(app).delete(`/api/users/${userId}`).set({ Authorization, reAuthorization })
+        expect(res.statusCode).toBe(204)
+    })
+})
+//     describe("get /api/users/:userId", () => {
+//         it("get /api/users/:userId 잘 되나?", async () => {
+//             const userId = "73"
+//             const res = await request(app)
+//                 .get(`/api/users/${userId}`)
+//                 .set(
+//                     "Authorization",
+//                     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjczLCJuaWNrbmFtZSI6IuydtO2RuOumhCIsInByb2ZpbGVVcmwiOiJodHRwOi8vay5rYWthb2Nkbi5uZXQvZG4vZHBrOWwxL2J0cW1HaEEybEtML096MHdEdUpuMVlWMkRJbjkyZjZEVksvaW1nXzY0MHg2NDAuanBnIiwiZW1haWwiOiJwb29yZXVtODNAZ21haWwuY29tIiwiaWF0IjoxNjUyNzk3NTkwLCJleHAiOjE2NTI4ODM5OTB9.jZxkdklHbQKx9xgeaLf8wMPJXRPzCsoGrxKuZ5lQkUo"
+//                 )
+//             expect(res.statusCode).toBe(201)
+//         })
+//     })
