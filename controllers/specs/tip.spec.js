4621106fdf8ec4546a50498461db5e62ee031a50
rlasuddn <rlasuddn>
2022-05-19 16:40:21 +0900

    feat(mark):북마크 delete api 추가
    2022.05.19 복세편살
    commit by youngwoo

--- controllers/specs/tip.spec.js
+++ controllers/specs/tip.spec.js
@@ -4,48 +4,78 @@ const tipController = require("../tip.controller")
 jest.mock("../../schemas/user")
 jest.mock("../../schemas/data")
 const User = require("../../schemas/user")
-const BokjiApi = require("../../schemas/data")
-const fs = require("fs")
+const BokjiAPI = require("../../schemas/data")
 
-let req, res, next, err, dataId, userId, dismatchData
+const dataId = { dataId: 1 }
+const userId = { userId: 1 }
+const mark = { mark: [1, 2, 3] }
+const dataIdList = [1, 2, 3, 4, 5]
+const dismatchData = { dismatchData: [1, 2, 3, 4] }
+
+let req, res, next, err
 beforeEach(() => {
     req = httpMocks.createRequest() //test코드에서 req, res를 사용하기 위해 httpMocks를 이용한다.
     res = httpMocks.createResponse()
     next = jest.fn()
-    dataId = 1
-    userId = 1
-    dismatchData = { dismatchData: [1, 2, 3, 4] }
 })
 
 describe("버그제보 테스트 코드", () => {
     describe("postTips 테스트", () => {
-        it("dataId가 없으면 에러를 뱉는가?", async () => {
-            dataId = undefined
-            req.body.dataId = dataId
-            res.locals.userId = userId
-            err = new Error("userId , dataId를 확인 해주세요")
-            const errMessage = { message: "userId , dataId를 확인 해주세요", stack: err }
-            await tipController.postTips(req, res, next)
-            expect(next).toBeCalledWith(errMessage)
+        it("postTips 함수인가?", async () => {
+            expect(true).toBe(true)
         })
-        it("userId가 없으면 에러를 뱉는가?", async () => {
-            userId = undefined
-            req.body.dataId = dataId
-            res.locals.userId = userId
-            err = new Error("userId , dataId를 확인 해주세요")
-            const errMessage = { message: "userId , dataId를 확인 해주세요", stack: err }
-            await tipController.postTips(req, res, next)
-            expect(next).toBeCalledWith(errMessage)
-        })
-        it("BokjiAPI.findOne이 없으면 에러를 뱉는가?", async () => {
-            req.body.dataId = dataId
-            res.locals.userId = userId
-            err = new Error("DB에 dataID가 없거나 이미 버그제보한 데이터입니다.")
-            const errMessage = { message: "DB에 dataID가 없거나 이미 버그제보한 데이터입니다.", stack: err }
-            BokjiApi.findOne.mockReturnValue(null)
-            await tipController.postTips(req, res, next)
-            expect(next).toBeCalledWith(errMessage)
-        })
+        // it("dataId가 없으면 에러를 뱉는가?", async () => {
+        //     dataId = undefined
+        //     req.body.dataId = dataId
+        //     res.locals.userId = userId
+        //     err = new Error("userId , dataId를 확인 해주세요")
+        //     const errMessage = { message: "userId , dataId를 확인 해주세요", stack: err }
+        //     await tipController.postTips(req, res, next)
+        //     expect(next).toBeCalledWith(errMessage)
+        // })
+        // it("userId가 없으면 에러를 뱉는가?", async () => {
+        //     userId = undefined
+        //     req.body.dataId = dataId
+        //     res.locals.userId = userId
+        //     err = new Error("userId , dataId를 확인 해주세요")
+        //     const errMessage = { message: "userId , dataId를 확인 해주세요", stack: err }
+        //     await tipController.postTips(req, res, next)
+        //     expect(next).toBeCalledWith(errMessage)
+        // })
+        // it("BokjiAPI.findOne이 잘 실행되는가?", async () => {
+        //     req.body.dataId = dataId
+        //     res.locals.userId = userId
+        //     const option = { _id: false, dataId: true }
+        //     await tipController.postTips(req, res, next)
+        //     expect(BokjiAPI.findOne).toBeCalledWith({ dataId }, option)
+        // })
+        // it("BokjiAPI.findOne이 없으면 에러를 뱉는가?", async () => {
+        //     req.body.dataId = dataId
+        //     res.locals.userId = userId
+        //     err = new Error("DB에 dataID가 없거나 이미 버그제보한 데이터입니다.")
+        //     const errMessage = { message: "DB에 dataID가 없거나 이미 버그제보한 데이터입니다.", stack: err }
+        //     BokjiAPI.findOne.mockReturnValue(null)
+        //     await tipController.postTips(req, res, next)
+        //     expect(next).toBeCalledWith(errMessage)
+        // })
+        // it("User.findOne이 잘 실행되는가?", async () => {
+        //     req.body.dataId = dataId
+        //     res.locals.userId = userId
+        //     const option = { _id: false, mark: true }
+        //     BokjiAPI.findOne.mockReturnValue(dataIdList)
+        //     await tipController.postTips(req, res, next)
+        //     expect(User.findOne).toBeCalledWith({ userId }, option)
+        // })
+        // it("Mark에 dataId가 있으면 User.updateOne을 잘 실행되는가?", async () => {
+        //     req.body.dataId = dataId
+        //     res.locals.userId = userId
+        //     const option = { $pullAll: { mark: [dataId] } }
+        //     BokjiAPI.findOne.mockReturnValue(dataIdList)
+        //     User.findOne.mockReturnValue(mark)
+        //     await tipController.postTips(req, res, next)
+        //     expect(User.updateOne).toBeCalledWith({ userId }, option)
+        // })
+
         // it("dismatchData에 이미 dataId가 있으면 에러를 뱉는가?", async () => {
         //     req.body.dataId = dataId
         //     res.locals.userId = userId
@@ -77,18 +107,6 @@ describe("버그제보 테스트 코드", () => {
         //     await tipController.postTips(req, res, next)
         //     expect(res.statusCode).toEqual(200)
         //     expect(res._getJSONData()).toStrictEqual(result)
-        // })
-        // it("postTips이 성공 했을때 writeFile이 잘 실행되는가?", async () => {
-        //     req.body.dataId = dataId
-        //     res.locals.userId = userId
-        //     dismatchData = { dismatchData: [2, 3, 4] }
-        //     BokjiApi.findOne.mockReturnValue(dataId)
-        //     User.findOne.mockReturnValue(dismatchData)
-        //     User.updateOne.mockReturnValue(dataId)
-        //     fs.writeFile = jest.fn()
-        //     await tipController.postTips(req, res, next)
-        //     expect(fs.writeFile).toHaveBeenCalledTimes(1)
-        //     fs.writeFile.mockClear()
         // })
     })
 })
