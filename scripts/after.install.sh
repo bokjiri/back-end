2f6cd947b1e742d1e92a7d3ab430563163f8585d
pooreumu <pooreum83@gmail.com>
2022-06-04 10:15:32 +0900

    feat(pm2): pm2 --watch 에서 reload로 변경
    pm2 --watch 에서 reload로 변경
    
    2022.06.04 복세편살
    commit by pooreumu

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -4,6 +4,7 @@ sudo chmod -R 777 /home/ubuntu/build
 #navigate into our working directory
 cd /home/ubuntu/build
 
-#install node modules & update swagger
+#install node modules & update swagger & pm2 reload
 sudo npm install
 node swagger.js
+sudo pm2 reload server
